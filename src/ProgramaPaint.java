3768edd8578e02aef1cd67ab7caf615e59ee539e
Javier <javirodriguez7@hotmail.es>
2019-04-23 13:17:54 +0200

    Clonaciones
    Creamos los bucles para clonar la imagen en distintas localizaciones

--- src/ProgramaPaint.java
+++ src/ProgramaPaint.java
@@ -15,17 +15,57 @@ public class ProgramaPaint {
 		public void leerYEscribirImagen() {
 			
 			try {
+				//Leo la imagen que ya existe del niño
+				BufferedImage ninioChucho=ImageIO.read(new File("./imagenes/niniochucho.jpg"));
 				
-				File archivoImagen=new File("./fondo.png");
-				BufferedImage imagen = ImageIO.read(archivoImagen);
-				System.out.println("SE HA LEÍDO CORRECTAMENTE LA IMAGEN");
-				ImageIO.write(imagen, "png", new File("./fondoCopia.png"));
-				System.out.println("SE HA ESCRITO LA IMAGEN");
+				//Imagen en blanco para duplicar
+				BufferedImage enBlanco=new BufferedImage(ninioChucho.getWidth()*2,ninioChucho.getHeight()*2,BufferedImage.TYPE_INT_RGB);
+
+				for(int i=0;i<enBlanco.getWidth();i++) {
+					for(int j=0;j<enBlanco.getHeight();j++) {
+						enBlanco.setRGB(i,j,Integer.parseInt("111111111111111111111111",2));
+						
+					}
+				}
+				
+				
+				//Los bucles siguientes son para clonar en diferentes posiciones.
+				
+				for(int i=0;i<ninioChucho.getWidth();i++) {
+					for(int j=0;j<ninioChucho.getHeight();j++) {
+						//enBlanco.setRGB(i,j,Integer.parseInt("111111111111111111111111",2));
+						enBlanco.setRGB(i, j, ninioChucho.getRGB(i, j));
+					}
+				}
+				for(int i=0;i<ninioChucho.getWidth();i++) {
+					for(int j=0;j<ninioChucho.getHeight();j++) {
+						//enBlanco.setRGB(i,j,Integer.parseInt("111111111111111111111111",2));
+						enBlanco.setRGB(i+ninioChucho.getWidth(), j, ninioChucho.getRGB(i, j));
+					}
+				}
+				for(int i=0;i<ninioChucho.getWidth();i++) {
+					for(int j=0;j<ninioChucho.getHeight();j++) {
+						//enBlanco.setRGB(i,j,Integer.parseInt("111111111111111111111111",2));
+						enBlanco.setRGB(i, j+ninioChucho.getHeight(), ninioChucho.getRGB(i, j));
+					}
+				}
+				for(int i=0;i<ninioChucho.getWidth();i++) {
+					for(int j=0;j<ninioChucho.getHeight();j++) {
+						//enBlanco.setRGB(i,j,Integer.parseInt("111111111111111111111111",2));
+						enBlanco.setRGB(i+ninioChucho.getWidth(), j+ninioChucho.getHeight(), ninioChucho.getRGB(i, j));
+					}
+				}
+				
+				
+				
+				
+				ImageIO.write(enBlanco,"png",new File("./imagenes/enBlanco.png"));
+				
 				
 				
 			}catch(Exception e) {
 				
-				
+				e.printStackTrace();
 			}
 		}
 		

ecb9ffc4278b98772cbdb7929ce70a72d275334e
Javier <javirodriguez7@hotmail.es>
2019-05-27 13:52:03 +0200

    Me acabo de dar cuenta que aqui no hay que poner el nombre
    Intento cambio para workbench casa y cenec

--- src/Conexiones/Constantes.java
+++ src/Conexiones/Constantes.java
@@ -1,7 +1,11 @@
 package Conexiones;
 
 public class Constantes {
+	
 		public final static String conexion="jdbc:mysql://127.0.0.1/bdPaint";
 		public final static String usuario="root";
-		public final static String contraseña="root";
+		public static String contraseña="";
+		
+		
 }
+
